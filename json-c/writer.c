dcff680de3fd9511c2d293d60627bd8049f47b33
Atul Sowani <atul_sowani@persistent.com>
2019-10-17 10:19:03 +0530

    json writer.

--- json-c/writer.c
+++ json-c/writer.c
@@ -10,16 +10,28 @@ int main (void)
   struct json_object *intObj1, *intObj2;
   time_t currtime;
 
+  const char *fname = "/home/test/tmp/fw_sandbox/fwsim/fwbackup/20191017-092135/result";
+  char line[20];
+  FILE *fp;
+  int val, ts;
+
+  fp = fopen (fname, "r");
+  fgets (&line[0], 20, fp);
+  fclose (fp);
+  sscanf (line, "%d:%d", &val, &ts);
+
   /* construct sample JSON */
   obj = json_object_new_object();
 
-  intObj1 = json_object_new_int (2);
+  intObj1 = json_object_new_int (val);
   retval = json_object_object_add (obj, "updateState", intObj1);
   currtime = time(NULL);
-  intObj2 = json_object_new_int ((int) time(NULL));
+  /* intObj2 = json_object_new_int ((int) time(NULL)); */
+  intObj2 = json_object_new_int (ts);
   retval = json_object_object_add (obj, "updateTime", intObj2);
 
   /* export sample JSON to file */
-  retval = json_object_to_file (filename, obj);
+  /* retval = json_object_to_file (filename, obj); */
+  retval = json_object_to_file_ext (filename, obj, JSON_C_TO_STRING_PRETTY);
   return (retval);
 }
